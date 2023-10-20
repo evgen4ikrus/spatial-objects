5014ed1cabe15eea7ef38f4b6ef6380291bfd8d3
Evgeniy Shesterikov <67272160+evgen4ikrus@users.noreply.github.com>
2023-10-20 23:48:27 +0300

    add dockerfile and docker-compose.yaml

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spatial_obgects_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spatial_objects_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- spatial_objects_project/wsgi.py
+++ spatial_objects_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for spatial_obgects_project project.
+WSGI config for spatial_objects_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spatial_obgects_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spatial_objects_project.settings')
 
 application = get_wsgi_application()
